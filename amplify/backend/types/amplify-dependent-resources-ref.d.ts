223eee44a58b21df8492e70a79791432f3c00b98
N-Dave-08 <cdave.dev@gmail.com>
2025-08-13 10:17:54 +0800

    chore: setup aws, amplify and install shadcn

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  auth: {
+    awssocialmediaapp9ed02540: {
+      AppClientID: 'string';
+      AppClientIDWeb: 'string';
+      IdentityPoolId: 'string';
+      IdentityPoolName: 'string';
+      UserPoolArn: 'string';
+      UserPoolId: 'string';
+      UserPoolName: 'string';
+    };
+  };
+};
